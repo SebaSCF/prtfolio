e843a7fb9a77e8a5d6b52bafb7f1eeaea7298da0
Sebastian <cappadonnasebastian10@gmail.com>
2021-05-24 13:45:32 -0500

    Added footer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Cv from "./Components/Cv";
 import React from 'react';
 import NavBar from "./Components/Navbar";
 import './App.css';
+import LowerNav from "./Components/LowerNav";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route component={Project} path="/Project" />
           <Route component={Cv} path="/CV"/>
         </Switch>
+        <LowerNav />
       </BrowserRouter>
     </div>
   );
